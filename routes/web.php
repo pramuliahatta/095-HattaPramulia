986ca8aaa2c3cc86ab1f00371bdd9b31fa29ba2e
pramuliahatta <pramulia.hatta@gmail.com>
2024-05-15 23:54:27 +0700

    Add tailwindcss, alpinejs, some new components. Edit routes, res/css.

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,50 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', [
+        'title' => 'Home',
+    ]);
+});
+Route::get('/post', function () {
+    return view('home', [
+        'title' => 'Your Posts',
+    ]);
+});
+Route::get('/post/{id}', function () {
+    return view('home', [
+        'title' => 'Your Posts',
+    ]);
+});
+Route::get('/profile', function () {
+    return view('home', [
+        'title' => 'Profile',
+    ]);
+});
+Route::get('/dashboard', function () {
+    return view('home', [
+        'title' => 'Dashboard',
+    ]);
+});
+Route::get('/dashboard/user', function () {
+    return view('home', [
+        'title' => 'User',
+    ]);
+});
+Route::get('/dashboard/user/{id}', function () {
+    return view('home', [
+        'title' => 'User',
+    ]);
+});
+Route::get('/dashboard/post', function () {
+    return view('home', [
+        'title' => 'Posts',
+    ]);
+});
+Route::get('/dashboard/post/{id}', function () {
+    return view('home', [
+        'title' => 'Posts',
+    ]);
+});
+Route::get('/logout', function () {
+    return 0;
 });
